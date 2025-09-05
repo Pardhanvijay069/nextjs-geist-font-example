e650c57391a37a4b27a294c812b6d6ba5a5f0953
Pardhanvijay069 <119775503+Pardhanvijay069@users.noreply.github.com>
2025-09-06 02:00:17 +0530

    Add .next/static/chunks/src_app_admin_customers_page_tsx_7fb79cf0._.js 1757104216545

---
+++ .next/static/chunks/src_app_admin_customers_page_tsx_7fb79cf0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_239d7b30._.js",
+  "static/chunks/node_modules_2dabffac._.js"
+],
+    source: "dynamic"
+});
